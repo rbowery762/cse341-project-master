20f604daefbe3dd2cf97065b925e5129cfe00ea3
rbowery762 <bow18018@byui.edu>
2020-09-24 13:23:59 -0500

    Team Activity 02

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,42 @@
 const express = require('express');
 const router = express.Router();
 
+const userList = ['Dragon', 'Cat', '99'];
+let errorMessage;
+
 router.get('/',(req, res, next) => {
     res.render('pages/ta02', { 
         title: 'Team Activity 02', 
         path: '/ta02', // For pug, EJS 
+        users: userList,
+        error: errorMessage,
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
     });
 });
 
-module.exports = router;
+router.post('/addUser',(req, res, next) => {
+    const newUserName = req.body.newUser;
+    if (userList.includes(newUserName)){
+        errorMessage = "Username already in use";
+    } else{
+        userList.push(newUserName);
+        errorMessage = null;
+    };
+    res.redirect('/ta02'); 
+
+});
+
+router.post('/removeUser',(req, res, next) => {
+    const removedUserName = req.body.removeUser;
+    const index = userList.indexOf(removedUserName);
+    if (index > -1){
+        userList.splice(index, 1);
+        errorMessage = null;
+    } else{
+        errorMessage ="User not found"; 
+    }
+    console.log(userList);
+    res.redirect('/ta02');
+});
+module.exports = router; 
