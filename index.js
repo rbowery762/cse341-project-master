ae84635f8b5ce1b2dc8c69b3faad83fc9e51c5d4
rbowery762 <bow18018@byui.edu>
2020-10-08 12:28:50 -0500

    Added Mongo support

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
 
 const app = express();
 
@@ -28,6 +29,35 @@ const ta01Routes = require('./routes/ta01C');
 const ta02Routes = require('./routes/ta02C');
 const ta03Routes = require('./routes/ta03C'); 
 const ta04Routes = require('./routes/ta04C'); 
+
+const corsOptions = {
+    origin: "https://<your_app_name>.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
+
+
+mongoose
+  .connect(
+    MONGODB_URL, options
+  )
+  .then(result => { // This should be your user handling code implement following the course videos
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  
 
 app.use(express.static(path.join(__dirname, 'public')))
    .set('views', path.join(__dirname, 'views'))
